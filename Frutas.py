b55ccf733672fcb80f377018ef124cefd09a5c74
209 <209@academicos.cesde.edu.co>
2022-09-10 12:56:03 -0500

    Se agrega los resultados de la evaluacion

---
+++ Frutas.py
@@ -0,0 +1,18 @@
+print("Diguite una fruta: ")
+print(frutas)
+print("Diguite el color: ")
+print(color1)
+
+
+frutas = ["bananno", "Manzana", "Pera", "Mango", "Zandia", "Fresa"]
+color1 = ["Rojo","Amarillo" , "Verde", "Rosa", "Negro"]
+
+
+frutas_ordenados = sorted(frutas, reverse=True)
+color1.sort(reverse=True)
+
+print("frutas ordenados: ")
+print(frutas_ordenados)
+print("color ordenadas: ")
+print(color1)
+
---
+++ numerosEnt.py
@@ -0,0 +1,11 @@
+
+b = int(input("Digite un numero: "))
+
+a = 1
+
+while a <= b:
+    if(a % 3) == 0:
+        print(a,end=" , ")
+    a = a + 1
+
+    
